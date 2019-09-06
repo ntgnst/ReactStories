9c131d9a344b82da4c18773a66d1763f1a1a09f6
ntgnst <ntgnst@github.com>
2019-09-06 15:58:40 +0300

    update

--- ghpage/src/App.js
+++ ghpage/src/App.js
@@ -1,11 +1,108 @@
-import React, { Component } from 'react'
+import React, { Component , setState} from 'react'
 import Stories from 'react-insta-stories'
-
+import StoryList from '../src/StoryList'
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pauseClicked : false
+      pauseClicked : false,
+      stories: [
+        {
+          url: 'https://picsum.photos/1080/1920',
+          seeMore: 'Detaylı Bak',
+          seeMoreText: 'AWDAWDAWD',
+          header:
+          {
+            heading: 'Batuhan Yahşi',
+            subheading: 'Posted 1m ago',
+            profileImage: 'https://avatars2.githubusercontent.com/u/17345531?s=460'
+          }
+        },
+        {
+          url: 'https://fsa.zobj.net/crop.php?r=dyJ08vhfPsUL3UkJ2aFaLo1LK5lhjA_5o6qEmWe7CW6P4bdk5Se2tYqxc8M3tcgYCwKp0IAyf0cmw9yCmOviFYb5JteeZgYClrug_bvSGgQxKGEUjH9H3s7PS9fQa3rpK3DN3nx-qA-mf6XN',
+          header:
+          {
+            heading: 'Batuhan Yahşi',
+            subheading: 'Posted 32m ago',
+            profileImage: 'https://avatars2.githubusercontent.com/u/17345531?s=460'
+          },
+          seeMore: 'Detaylı Bak',
+          seeMoreText: 'ASDASDAWD'
+        },
+        {
+          url: 'https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg',
+          header:
+          {
+            heading: 'Batuhan Yahşi',
+            subheading: 'Posted 32m ago',
+            profileImage: 'https://avatars2.githubusercontent.com/u/17345531?s=460'
+          },
+          seeMore: 'Detaylı Bak',
+          seeMoreText: 'DAHA FAZLA GÖR'
+        },
+        {
+          url: 'https://storage.googleapis.com/coverr-main/mp4/Footboys.mp4',
+          type: 'video',
+          duration: 1000,
+          seeMore: 'Detaylı Bak',
+          seeMoreText: 'DAHA FAZLA GÖR',
+          header:
+          {
+            heading: 'Batuhan Yahşi',
+            subheading: 'Posted 34m ago',
+            profileImage: 'https://avatars2.githubusercontent.com/u/17345531?s=460'
+          }
+        },
+        {
+          url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
+          type: 'video',
+          seeMore: 'Detaylı Bak',
+          seeMoreText: 'DAHA FAZLA GÖR',
+          header:
+          {
+            heading: 'Batuhan Yahşi',
+            subheading: 'Posted 36m ago',
+            profileImage: 'https://avatars2.githubusercontent.com/u/17345531?s=460'
+          }
+        },
+        {
+          url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+          type: 'video',
+          seeMore: 'Detaylı Bak',
+          seeMoreText: 'DAHA FAZLA GÖR',
+          header:
+          {
+            heading: 'Batuhan Yahşi',
+            subheading: 'Posted 45m ago',
+            profileImage: 'https://avatars2.githubusercontent.com/u/17345531?s=460'
+          },
+        },
+        'https://images.unsplash.com/photo-1534856966153-c86d43d53fe0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80',
+        'https://images.unsplash.com/photo-1508186225823-0963cf9ab0de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+        'https://images.unsplash.com/photo-1499202189329-5d76e29aa2b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80'
+      ].map((s, i) => {
+        if (i !== 4 && i !== 5) {
+          if (typeof s === 'string') {
+            let o = {}
+            o.url = s
+            o.header = {
+              heading: 'Unknown',
+              subheading: 'Unknown',
+              profileImage: 'http://www.baytekent.com/wp-content/uploads/2016/12/facebook-default-no-profile-pic1.jpg'
+            }
+            o.styles = {
+              objectFit: 'cover',
+              width: '100%', margin: 'none'
+            }
+            return o
+          }
+          s.styles = {
+            objectFit: 'cover',
+            width: '100%', margin: 'none'
+          }
+          return s
+        } else return s
+      })
     }
     this.stories = React.createRef();
     this.col = React.createRef();
@@ -17,7 +114,6 @@ export default class App extends Component {
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
   }
-
   play = () => {
     this.stories.current.play();
     this.setState({pauseClicked: false});
@@ -32,6 +128,13 @@ export default class App extends Component {
   next = () => {
     this.stories.current.next();
   }
+  reverse = () => {
+    this.setState({stories: this.state.stories.reverse()});
+  }
+  storySelectedHandler = (el,i) => {
+    // let temp = Object.assign([],this.state.stories);
+    //this.setState({stories : temp.splice(i,0,el)});
+  }
   handleScroll = () => {
     if (this.col.clientHeight - window.pageYOffset > 0) {
       if(!this.state.pauseClicked){
@@ -45,16 +148,24 @@ export default class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <div style={{paddingLeft: '1%',paddingRight: '1%',paddingBottom: '0.5%', backgroundColor: 'rgba(0, 0, 0, 0.8)'}}>
+        <StoryList stories={this.state.stories} storySelected={(el,i) => this.storySelectedHandler(el,i)} />
+        </div>
       <div className="App">
+        
+        <div style={{textAlign:"center !important"}}>
+          
         <div className="stories" onScroll={this.handleScroll} ref={(col) => {this.col = col}}>
-          <Stories ref={this.stories} stories={stories} defaultDuration={3000} />
+          <Stories ref={this.stories} stories={this.state.stories} defaultDuration={3000} />
           <div style={{ textAlign: 'center' }}>
             <button onClick={this.pause}>Duraklat</button>
             <button onClick={this.play}>Başlat</button>
             <button onClick={this.prev}> <span> {"<"} </span> </button>
             {"|"}
             <button onClick={this.next}><span>{">"}</span></button>
+            <button onClick={this.reverse}><span>{"reverse flow"}</span></button>
           </div>
+        </div>
         </div>
       </div >
       <div>
@@ -254,6 +365,11 @@ const stories = [
     if (typeof s === 'string') {
       let o = {}
       o.url = s
+      o.header = {
+        heading: 'Unknown',
+        subheading: 'Unknown',
+        profileImage: 'http://www.baytekent.com/wp-content/uploads/2016/12/facebook-default-no-profile-pic1.jpg'
+      }
       o.styles = {
         objectFit: 'cover',
         width: '100%', margin: 'none'
